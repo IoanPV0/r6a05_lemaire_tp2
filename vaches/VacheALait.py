07aab64b5560dc6cc92a03e7d4f0c37f5f119592
IoanPV0 <166406667+IoanPV0@users.noreply.github.com>
2026-02-03 16:52:40 +0000

    cmt rdm

--- vaches/VacheALait.py
+++ vaches/VacheALait.py
@@ -4,10 +4,17 @@ from vaches.Vache import Vache
 class VacheALait(Vache):
     RENDEMENT_LAIT = 1.1
     PRODUCTION_LAIT_MAX = 40.0
-    def __init__(self, petit_nom:str = None, age:int = None, poids:float = None, panse:float = 0.0) -> None:
-        super().__init__(petit_nom=petit_nom, age=age, poids=poids, panse=panse)
+    def __init__(self, petit_nom:str = None, age:int = None, poids:float = None) -> None:
+        super().__init__(petit_nom = petit_nom, age = age, poids = poids, panse = 0.0)
         self._lait_disponible = 0.0
         self._lait_total_produit = 0.0
         self._lait_total_traite = 0.0
 
+    @property
+    def lait_disponible(self) -> float:
+        return self._lait_disponible
     
+    def ruminer(self):
+        super().ruminer()
+        lait_produit = VacheALait.RENDEMENT_LAIT * (panse_avant)
+        self._lait_disponible += lait_produit
