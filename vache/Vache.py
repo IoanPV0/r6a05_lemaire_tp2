4fbbfdad83b0c5dc1e9dd1c021663c63d32ca41d
IoanPV0 <166406667+IoanPV0@users.noreply.github.com>
2026-02-12 13:35:58 +0000

    cht non valides

--- vache/Vache.py
+++
@@ -1,8 +0,0 @@
-class Vache:
-    def __init__(self, petit_nom:str = None, age:int = None, poids:float = None, panse:float = None):
-        self._petit_nom = petit_nom
-        self._age = age
-        self._poids = poids
-        self._panse = panse
-
-    
