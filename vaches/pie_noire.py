8627ead480a246a5ed6b282e9748ede2010edf4a
IoanPV0 <166406667+IoanPV0@users.noreply.github.com>
2026-02-12 14:35:19 +0000

    Mise a jour pienoire et autre

--- vaches/pie_noire.py
+++ vaches/pie_noire.py
@@ -3,7 +3,7 @@ from vaches.vache import Vache, VacheALait
 from nourriture.type_nourriture import TypeNourriture
 
 class PieNoire(VacheALait):
-    COEFFICIENT_LAIT_PAR_NOURRITURE = dict[type_nourriture, float]
+    COEFFICIENT_LAIT_PAR_NOURRITURE = dict[TypeNourriture, float]
     def __init__(self, petit_nom:str = None, age:int = None, poids:float = None, nombre_tache_blanche:int = None, nombre_tache_noire:int = None) -> None:
         super().__init__(petit_nom = petit_nom, age = age, poids = poids)
 
